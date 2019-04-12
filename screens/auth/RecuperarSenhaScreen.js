a8c36d87e09e32c9687ace6f9385cb359160d4b2
unknown <jeanmdasleite@gmail.com>
2019-04-11 20:32:42 -0400

    login

---
+++ screens/auth/RecuperarSenhaScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+export default class RecuperarSenhaScreen extends React.Component {
+    render() {
+    return (
+      <View >
+        <Text>RecuperarSenhaScreen</Text>
+      </View>
+    );
+  }
+};
---
+++ screens/auth/RegistroScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+export default class RegistroScreen extends React.Component {
+    render() {
+    return (
+      <View >
+        <Text>RegistroScreen</Text>
+      </View>
+    );
+  }
+};
